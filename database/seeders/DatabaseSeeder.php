4e54e29145328e9f87445184ec3df3ceda0f4333
sahrull-mahani <sahrul.mahani@gmail.com>
2022-02-23 21:54:47 +0800

    mengubah/menambah file seeder 21:55 23Feb22
    +models
      -mengubah post dan user model
    +migrations
      -mengubah file post migration
    +seeder
      -mengubah database seeder
    +views
      -mengubah post dan posts view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name'      => "Sahrul Mahani",
+            'email'     => "sahrul@gmail.com",
+            'password'  => bcrypt('123456')
+        ]);
+
+        User::create([
+            'name'      => "Maman Pauweni",
+            'email'     => "maman@gmail.com",
+            'password'  => bcrypt('123456')
+        ]);
+
+        Category::create([
+            'name'  => "Web Programming",
+            'slug'  => "web-programming"
+        ]);
+
+        Category::create([
+            'name'  => "Personal",
+            'slug'  => "personal"
+        ]);
+
+        Post::create([
+            'title'     => "Judul Pertama",
+            'slug'      => "judul-pertama",
+            'excerpt'   => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam distinctio",
+            'body'      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam distinctio itaque dolorem quam omnis maiores nisi? Quisquam, voluptates. Deserunt illo officia in fugit unde quisquam aspernatur sit a? Facilis asperiores dicta perferendis doloribus consequatur, aut quibusdam delectus consectetur dolores dolorum molestiae soluta, pariatur alias ea ab. Illo adipisci suscipit dicta dolores autem nihil aspernatur explicabo labore ad aliquid quisquam, nostrum voluptatum. Cum exercitationem sequi maxime, delectus doloribus ratione aliquid. Placeat assumenda voluptatum laudantium asperiores consectetur dolorem non doloremque vero quod perferendis veritatis omnis, nostrum est exercitationem! Fuga at vel ipsa, optio laudantium veritatis quisquam voluptates nulla molestias aut non repellendus?",
+            'category_id'   => 1,
+            'user_id'       => 2
+        ]);
+
+        Post::create([
+            'title'     => "Judul Kedua",
+            'slug'      => "judul-kedua",
+            'excerpt'   => "Lorem ipsum kedua dolor, sit amet consectetur adipisicing elit. Quisquam distinctio",
+            'body'      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam distinctio itaque dolorem quam omnis maiores nisi? Quisquam, voluptates. Deserunt illo officia in fugit unde quisquam aspernatur sit a? Facilis asperiores dicta perferendis doloribus consequatur, aut quibusdam delectus consectetur dolores dolorum molestiae soluta, pariatur alias ea ab. Illo adipisci suscipit dicta dolores autem nihil aspernatur explicabo labore ad aliquid quisquam, nostrum voluptatum. Cum exercitationem sequi maxime, delectus doloribus ratione aliquid. Placeat assumenda voluptatum laudantium asperiores consectetur dolorem non doloremque vero quod perferendis veritatis omnis, nostrum est exercitationem! Fuga at vel ipsa, optio laudantium veritatis quisquam voluptates nulla molestias aut non repellendus?",
+            'category_id'   => 1,
+            'user_id'       => 1
+        ]);
+
+        Post::create([
+            'title'     => "Judul Ketiga",
+            'slug'      => "judul-ketiga",
+            'excerpt'   => "Lorem ipsum ketiga dolor, sit amet consectetur adipisicing elit. Quisquam distinctio",
+            'body'      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam distinctio itaque dolorem quam omnis maiores nisi? Quisquam, voluptates. Deserunt illo officia in fugit unde quisquam aspernatur sit a? Facilis asperiores dicta perferendis doloribus consequatur, aut quibusdam delectus consectetur dolores dolorum molestiae soluta, pariatur alias ea ab. Illo adipisci suscipit dicta dolores autem nihil aspernatur explicabo labore ad aliquid quisquam, nostrum voluptatum. Cum exercitationem sequi maxime, delectus doloribus ratione aliquid. Placeat assumenda voluptatum laudantium asperiores consectetur dolorem non doloremque vero quod perferendis veritatis omnis, nostrum est exercitationem! Fuga at vel ipsa, optio laudantium veritatis quisquam voluptates nulla molestias aut non repellendus?",
+            'category_id'   => 2,
+            'user_id'       => 1
+        ]);
+
+        Post::create([
+            'title'     => "Judul Keempat",
+            'slug'      => "judul-keempat",
+            'excerpt'   => "Lorem ipsum keempat dolor, sit amet consectetur adipisicing elit. Quisquam distinctio",
+            'body'      => "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam distinctio itaque dolorem quam omnis maiores nisi? Quisquam, voluptates. Deserunt illo officia in fugit unde quisquam aspernatur sit a? Facilis asperiores dicta perferendis doloribus consequatur, aut quibusdam delectus consectetur dolores dolorum molestiae soluta, pariatur alias ea ab. Illo adipisci suscipit dicta dolores autem nihil aspernatur explicabo labore ad aliquid quisquam, nostrum voluptatum. Cum exercitationem sequi maxime, delectus doloribus ratione aliquid. Placeat assumenda voluptatum laudantium asperiores consectetur dolorem non doloremque vero quod perferendis veritatis omnis, nostrum est exercitationem! Fuga at vel ipsa, optio laudantium veritatis quisquam voluptates nulla molestias aut non repellendus?",
+            'category_id'   => 1,
+            'user_id'       => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,9 @@
 @section('container')
   <article class="border rounded p-2">
     <h2>{{ $post->title }}</h2>
-    <p>By: Sahrul Mahani in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+    <p>By: <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 
-    <a href="/blog" class="d-block">Back to post's</a>
+    <a href="/blog" class="d-block text-decoration-none">Back to post's</a>
   </article>
 @endsection
