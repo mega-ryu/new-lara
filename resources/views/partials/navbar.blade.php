8b202a6427d0e9ce51b5bd0e918f325f29eac91e
sahrull-mahani <sahrul.mahani@gmail.com>
2022-02-25 21:38:57 +0800

    Membuat tampilan login dan register
    +controllers
      -menambahkan login dan register controller
    +routes
      -menambahkan route baru [login dan register]
    +views
      -menambahkan tampilan register dan login
      -mengubah tampilan navbar dan main

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-danger">
+{{-- <nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-danger">
   <div class="container">
     <a class="navbar-brand" href="/">MSM Blog</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
@@ -7,11 +7,45 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link {{ ($active === "home") ? 'active' : '' }}" aria-current="page" href="/">Home</a>
-        <a class="nav-link {{ ($active === "about") ? 'active' : '' }}" href="/about">About</a>
-        <a class="nav-link {{ ($active === "posts") ? 'active' : '' }}" href="/blog">Post's</a>
-        <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
+        <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" aria-current="page" href="/">Home</a>
+        <a class="nav-link {{ $active === 'about' ? 'active' : '' }}" href="/about">About</a>
+        <a class="nav-link {{ $active === 'posts' ? 'active' : '' }}" href="/blog">Post's</a>
+        <a class="nav-link {{ $active === 'categories' ? 'active' : '' }}" href="/categories">Categories</a>
       </div>
+    </div>
+  </div>
+</nav> --}}
+
+<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link {{ $active === 'home' ? 'active' : '' }}" aria-current="page" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ $active === 'about' ? 'active' : '' }}" href="/about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ $active === 'posts' ? 'active' : '' }}" href="/blog">Post's</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ $active === 'categories' ? 'active' : '' }}" href="/categories">Categories</a>
+        </li>
+      </ul>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="/login" class="nav-link {{ $active === 'login' ? 'active' : '' }}">Login <i class="bi bi-box-arrow-in-right"></i></a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
